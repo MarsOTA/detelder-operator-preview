84a6e347e50cb1330f61b192305954ab0b99ea17
MarsOTA <97559581+MarsOTA@users.noreply.github.com>
2026-09-05 16:44:07 +0200

    Use developer CSS path

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './preview.css';
-import './styleOperatori.css';
+import './pages/common/styleOperatori.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
